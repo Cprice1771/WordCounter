1bbdc2762daa5febb62e697a3d574b71b1450590
Cprice <thrashers231@yahoo.com>
2014-09-08 17:34:40 -0400

    initial commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText("Filename.txt"))
+            {
+                string file = reader.ReadToEnd();
+                string[] words = file.Split(' ');
+                Dictionary<string, int> wordCount = new Dictionary<string, int>();
+                foreach (string s in words)
+                {
+                    if (wordCount.ContainsKey(s))
+                        wordCount[s]++;
+                    else
+                        wordCount.Add(s, 1);
+                }
+
+                foreach (KeyValuePair<string, int> kvp in wordCount)
+                {
+                    Console.WriteLine(kvp.Key + ", " + kvp.Value.ToString());
+                }
+            }
+
+            
+        }
+    }
+}
